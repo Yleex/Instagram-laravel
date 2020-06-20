f317faf709638356f0cf416f7aefa39efbd6d6b7
Yleex <leex.cm@gmail.com>
2020-06-20 07:40:56 -0400

    agregando-los-modelos

--- database.sql
+++ database.sql
@@ -13,39 +13,39 @@ image						varchar(255),
 created_at			datetime,
 updated_at			datetime,
 remenber_token	varchar(255),
-CONSTRAINT pk_user PRYMARY KEY(id)
+CONSTRAINT pk_users PRIMARY KEY(id)
 )ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS images(
 id 							int(255) auto_increment not null,
-user_id					int(255),
+users_id					int(255),
 image_path			varchar(255),
 description			text,
 created_at			datetime,
 updated_at			datetime,
-CONSTRAINT pk_images PRYMARY KEY(id),
-CONSTRAINT fk_images_users FOREING KEY(user_id) REFERENCES users(id)
+CONSTRAINT pk_images PRIMARY KEY(id),
+CONSTRAINT fk_images_users FOREIGN KEY(users_id) REFERENCES users(id)
 )ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS comments(
 id 							int(255) auto_increment not null,
-user_id					int(255),
+users_id					int(255),
 image_id				int(255),
 content					text,
 created_at			datetime,
 updated_at			datetime,
-CONSTRAINT pk_comments PRYMARY KEY(id),
-CONSTRAINT fk_comments_users FOREING KEY(user_id) REFERENCES users(id),
-CONSTRAINT fk_comments_images FOREING KEY(image_id) REFERENCES images(id)
+CONSTRAINT pk_comments PRIMARY KEY(id),
+CONSTRAINT fk_comments_users FOREIGN KEY(users_id) REFERENCES users(id),
+CONSTRAINT fk_comments_images FOREIGN KEY(image_id) REFERENCES images(id)
 )ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS likes(
 id 							int(255) auto_increment not null,
-user_id					int(255),
+users_id					int(255),
 image_id				int(255),
 created_at			datetime,
 updated_at			datetime,
-CONSTRAINT pk_likes PRYMARY KEY(id),
-CONSTRAINT fk_likes_users FOREING KEY(user_id) REFERENCES users(id),
-CONSTRAINT fk_likes_images FOREING KEY(image_id) REFERENCES images(id)
+CONSTRAINT pk_likes PRIMARY KEY(id),
+CONSTRAINT fk_likes_users FOREIGN KEY(users_id) REFERENCES users(id),
+CONSTRAINT fk_likes_images FOREIGN KEY(image_id) REFERENCES images(id)
 )ENGINE=InnoDB;
